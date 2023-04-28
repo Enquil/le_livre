61b02c8a1a844f7dd8dc17382eb337ed0764705b
Enquil <jim.e.olesen@gmail.com>
2023-04-28 11:06:47 +0000

    set debug to false

--- le_livre/settings.py
+++ le_livre/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-!lmabmzj6!ei&(gb!9mh1dj-(*&8ram0*p5bf%##k*c0tz-x7n'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['le-livre.herokuapp.com', 'localhost']
 
